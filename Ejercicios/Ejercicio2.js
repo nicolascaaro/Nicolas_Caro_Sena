ffaf1003c83784306187dbfab84c4edd981fdd59
nicolascaaro <ncaro7962@gmail.com>
2025-04-24 09:08:36 -0500

    Delete Ejercicios directory
    delete

--- Ejercicios/Ejercicio2.js
+++
@@ -1,7 +0,0 @@
-let numero = 4
-
-if (numero %2 === 0){
-    console.log ("numero par")
-}else {
-    console.log ("numero impar")
-}
--- Ejercicios/Ejercicio4.js
+++
@@ -1,8 +0,0 @@
-let usuario = "nicolas"
-let numero = 7
-
-if (numero %2 === 0){
-    console.log(`${usuario} el numero que ha ingresado es par`);
-} else {
-    console.log(`${usuario} el numero que ha ingresado es impar`);
-}
